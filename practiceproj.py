e3233aa4c965a3402d109546f9d1c85dd0a236cb
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2023-08-01 18:57:40 -0400

    Update practiceproj.py
    added some conditions

--- practiceproj.py
+++ practiceproj.py
@@ -1,25 +1,30 @@
 import random
 def guess_game():
-    lower_bound=int(input("enter the LOWER bound for your game: "))
-    upper_bound=int(input("enter the UPPER bound for your game: "))
-    rand_num=random.randint(lower_bound,upper_bound)
-    attempts=0
     while True:
-        try:
-            guess_num=int(input(f'enter a number beatween {lower_bound} and {upper_bound} :'))
-        except ValueError:
-            print("Invalid input, try someother number")
-            continue
-        attempts+=1
-        if guess_num<lower_bound or guess_num>upper_bound:
-            print("you number is not in the range of {lower_bound} and {upper_bound} ")
-        elif(guess_num<rand_num):
-            guess_num=int(input("Try entering some higher num: "))
-        elif(guess_num>rand_num):
-            guess_num=int(input("Try entering some lower num: "))
-        else:
-            print(f"Congrats!You won nothing for guessing correct num{rand_num}")
+        lower_bound=int(input("enter the LOWER bound for your game: "))
+        upper_bound=int(input("enter the UPPER bound for your game: "))
+        rand_num=random.randint(lower_bound,upper_bound)
+        attempts=0
+        while True:
+            try:
+                guess_num=int(input(f'enter a number beatween {lower_bound} and {upper_bound} :'))
+            except ValueError:
+                print("Invalid input, try someother number")
+                continue
+            attempts+=1
+            if guess_num<lower_bound or guess_num>upper_bound:
+                print(f"you number is not in the range of {lower_bound} and {upper_bound} ")
+            elif(guess_num<rand_num):
+                guess_num=int(input("Try entering some higher num: "))
+            elif(guess_num>rand_num):
+                guess_num=int(input("Try entering some lower num: "))
+            else:
+                print(f"Congrats!You won nothing for guessing correct num{rand_num}")
+                break
+        print(f'total attempts: {attempts}')
+        play_again=input("If you want to waste your time again? (yes/y): ").lower()
+        if(play_again not in ['yes','y']):
+            print("Good Decision!!Go do some userful work")
             break
-    print(f'total attempts: {attempts}')
 if __name__=="__main__":
     guess_game()       
