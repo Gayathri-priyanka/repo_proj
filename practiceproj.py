a1f50bd360860cc10ecf92effb516627de14b0c7
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2023-08-01 18:35:48 -0400

    Update practiceproj.py

--- practiceproj.py
+++ practiceproj.py
@@ -1,17 +1,19 @@
 import random
 def guess_game():
-    lower_bound=1
-    upper_bound=10
+    lower_bound=int(input("enter the LOWER bound for your game: "))
+    upper_bound=int(input("enter the UPPER bound for your game: "))
     rand_num=random.randint(lower_bound,upper_bound)
     attempts=0
     while True:
         try:
-            guess_num=int(input('enter a number beatween 1 and 10: '))
+            guess_num=int(input(f'enter a number beatween {lower_bound} and {upper_bound} :'))
         except ValueError:
             print("Invalid input, try someother number")
             continue
         attempts+=1
-        if(guess_num<rand_num):
+        if guess_num<lower_bound or guess_num>upper_bound:
+            print("you number is not in the range of {lower_bound} and {upper_bound} ")
+        elif(guess_num<rand_num):
             guess_num=int(input("Try entering some higher num: "))
         elif(guess_num>rand_num):
             guess_num=int(input("Try entering some lower num: "))
