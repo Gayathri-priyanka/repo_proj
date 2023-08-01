3a54904e3abac125aa830f9ce5b83a8bf73b35b6
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2023-08-01 18:06:59 -0400

    Update practiceproj.py

--- practiceproj.py
+++ practiceproj.py
@@ -1,13 +1,23 @@
 import random
-rand_num=random.randint(1,10)
-guess_num=int(input('enter a number beatween 1 and 10: '))
-while guess_num!=rand_num:
-    if(guess_num<rand_num):
-        guess_num=int(input("Try entering some higher num: "))
-    elif(guess_num>rand_num):
-        guess_num=int(input("Try entering some lower num: "))
-    else:
-        print("You entered correct number")
-        break
-
-        
+def guess_game():
+    lower_bound=1
+    upper_bound=10
+    rand_num=random.randint(lower_bound,upper_bound)
+    attempts=0
+    while True:
+        try:
+            guess_num=int(input('enter a number beatween 1 and 10: '))
+        except ValueError:
+            print("Invalid input, try someother number")
+            continue
+        attempts+=1
+        if(guess_num<rand_num):
+            guess_num=int(input("Try entering some higher num: "))
+        elif(guess_num>rand_num):
+            guess_num=int(input("Try entering some lower num: "))
+        else:
+            print(f"Congrats!You won nothing for guessing correct num{rand_num}")
+            break
+    print(f'total attempts: {attempts}')
+if __name__=="__main__":
+    guess_game()       
