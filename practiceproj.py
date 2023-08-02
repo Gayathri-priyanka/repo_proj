188848ed9077f06aece6953752d29e419ddd4fcd
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2023-08-02 18:30:07 -0400

    Update practiceproj.py
    setting limits

--- practiceproj.py
+++ practiceproj.py
@@ -5,26 +5,36 @@ def guess_game():
         upper_bound=int(input("enter the UPPER bound for your game: "))
         rand_num=random.randint(lower_bound,upper_bound)
         attempts=0
-        while True:
+        max_attempts=5
+        while attempts<max_attempts:
             try:
                 guess_num=int(input(f'enter a number beatween {lower_bound} and {upper_bound} :'))
+                
+
             except ValueError:
                 print("Invalid input, try someother number")
                 continue
-            attempts+=1
+           
+
             if guess_num<lower_bound or guess_num>upper_bound:
                 print(f"you number is not in the range of {lower_bound} and {upper_bound} ")
             elif(guess_num<rand_num):
-                guess_num=int(input("Try entering some higher num: "))
+                print("Try entering some higher num: ")
+                attempts+=1
             elif(guess_num>rand_num):
-                guess_num=int(input("Try entering some lower num: "))
+                print("Try entering some lower num: ")
+                attempts+=1
             else:
                 print(f"Congrats!You won nothing for guessing correct num{rand_num}")
+                attempts+=1
                 break
+            
         print(f'total attempts: {attempts}')
-        play_again=input("If you want to waste your time again? (yes/y): ").lower()
+        play_again=input("If you want to play again? (yes/y):  ").lower()
         if(play_again not in ['yes','y']):
-            print("Good Decision!!Go do some userful work")
+            print("Thanks for playing! Have a great day!")
             break
+        else:
+            print(f"You have reached maximum num of attempts{max_attempts}. The correct num was {rand_num}")
 if __name__=="__main__":
     guess_game()       
